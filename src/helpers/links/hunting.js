05c40f8841c95a86f952075d17559077d83fb000
Andrewf9001 <andrewf9001@gmail.com>
2025-05-13 15:30:31 -0600

    added all subcategories to hunting section

--- src/helpers/links/hunting.js
+++ src/helpers/links/hunting.js
@@ -1,52 +1,388 @@
 const huntingLinkData = {
   link: "Hunting",
   subCategory: {
-    title: "",
-    links: [],
+    title: "Archery",
+    links: [
+      {
+        link: "#",
+        text: "Bows",
+      },
+      {
+        link: "#",
+        text: "Compound Bows",
+      },
+      {
+        link: "#",
+        text: "Crossbows",
+      },
+      {
+        link: "#",
+        text: "Arrows",
+      },
+      {
+        link: "#",
+        text: "Points & Broadheads",
+      },
+      {
+        link: "#",
+        text: "Archery Targets",
+      },
+      {
+        link: "#",
+        text: "Archery Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Hunting Packs",
+    links: [
+      {
+        link: "#",
+        text: "Expedition Packs",
+      },
+      {
+        link: "#",
+        text: "Day Packs",
+      },
+      {
+        link: "#",
+        text: "Frame Packs",
+      },
+      {
+        link: "#",
+        text: "Hydration Packs",
+      },
+      {
+        link: "#",
+        text: "Lumbar Packs",
+      },
+      {
+        link: "#",
+        text: "Bino Harness",
+      },
+      {
+        link: "#",
+        text: "Pack Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Blinds",
+    links: [
+      {
+        link: "#",
+        text: "Game Blinds",
+      },
+      {
+        link: "#",
+        text: "Blind Chairs",
+      },
+      {
+        link: "#",
+        text: "Layout Blinds",
+      },
+      {
+        link: "#",
+        text: "Waterfowl Blinds",
+      },
+      {
+        link: "#",
+        text: "Blind Bags",
+      },
+      {
+        link: "#",
+        text: "Blind Cover",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Treestands",
+    links: [
+      {
+        link: "#",
+        text: "Ladder Stands",
+      },
+      {
+        link: "#",
+        text: "Hang On Stands",
+      },
+      {
+        link: "#",
+        text: "Climbing Stands",
+      },
+      {
+        link: "#",
+        text: "Tower Stands",
+      },
+      {
+        link: "#",
+        text: "Safety",
+      },
+      {
+        link: "#",
+        text: "Ladders & Climbing Sticks",
+      },
+      {
+        link: "#",
+        text: "Hangers & Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Optics",
+    links: [
+      {
+        link: "#",
+        text: "Rifle Scopes",
+      },
+      {
+        link: "#",
+        text: "Binoculars",
+      },
+      {
+        link: "#",
+        text: "Spotting Scopes",
+      },
+      {
+        link: "#",
+        text: "Rangefinder",
+      },
+      {
+        link: "#",
+        text: "Tripods & Mounts",
+      },
+      {
+        link: "#",
+        text: "Scope Rings & Bases",
+      },
+      {
+        link: "#",
+        text: "View All",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Calls",
+    links: [
+      {
+        link: "#",
+        text: "Duck",
+      },
+      {
+        link: "#",
+        text: "Turkey",
+      },
+      {
+        link: "#",
+        text: "Elk",
+      },
+      {
+        link: "#",
+        text: "Deer",
+      },
+      {
+        link: "#",
+        text: "Predator",
+      },
+      {
+        link: "#",
+        text: "Call Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Decoys",
+    links: [
+      {
+        link: "#",
+        text: "Waterfowl",
+      },
+      {
+        link: "#",
+        text: "Upland",
+      },
+      {
+        link: "#",
+        text: "Turkey",
+      },
+      {
+        link: "#",
+        text: "Big Game",
+      },
+      {
+        link: "#",
+        text: "Predator",
+      },
+      {
+        link: "#",
+        text: "Decoy Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Hunting Accessories",
+    links: [
+      {
+        link: "#",
+        text: "Trail Cameras",
+      },
+      {
+        link: "#",
+        text: "GPS & Navigation",
+      },
+      {
+        link: "#",
+        text: "Two-Way Radios",
+      },
+      {
+        link: "#",
+        text: "Scent & Scent Eliminaton",
+      },
+      {
+        link: "#",
+        text: "Food & Attractants",
+      },
+      {
+        link: "#",
+        text: "Hunting Knives",
+      },
+      {
+        link: "#",
+        text: "View All",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Game Processing",
+    links: [
+      {
+        link: "#",
+        text: "Meat Grinders & Slicers",
+      },
+      {
+        link: "#",
+        text: "Vacuum Sealers",
+      },
+      {
+        link: "#",
+        text: "Dehydrators",
+      },
+      {
+        link: "#",
+        text: "Gambrels & Hoists",
+      },
+      {
+        link: "#",
+        text: "Game Bags",
+      },
+      {
+        link: "#",
+        text: "Accessories",
+      },
+      {
+        link: "#",
+        text: "View All",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Dog Supplies",
+    links: [
+      {
+        link: "#",
+        text: "Electronic Collars",
+      },
+      {
+        link: "#",
+        text: "Crates & Beds",
+      },
+      {
+        link: "#",
+        text: "Collars & Wearables",
+      },
+      {
+        link: "#",
+        text: "Dog Care",
+      },
+      {
+        link: "#",
+        text: "Training Aids",
+      },
+      {
+        link: "#",
+        text: "Dog Toys",
+      },
+      {
+        link: "#",
+        text: "Dog Food & Treats",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Hunting Clothing",
+    links: [
+      {
+        link: "#",
+        text: "Jackets & Vests",
+      },
+      {
+        link: "#",
+        text: "Shirts",
+      },
+      {
+        link: "#",
+        text: "Pants",
+      },
+      {
+        link: "#",
+        text: "Waders",
+      },
+      {
+        link: "#",
+        text: "Blaze Orange",
+      },
+      {
+        link: "#",
+        text: "Boots",
+      },
+      {
+        link: "#",
+        text: "Camo Finder",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Specialty",
+    links: [
+      {
+        link: "#",
+        text: "Waterfowl",
+      },
+      {
+        link: "#",
+        text: "Upland",
+      },
+      {
+        link: "#",
+        text: "Turkey",
+      },
+      {
+        link: "#",
+        text: "Big Game",
+      },
+      {
+        link: "#",
+        text: "Predator",
+      },
+      {
+        link: "#",
+        text: "Trapping",
+      },
+      {
+        link: "#",
+        text: "Bow Fishing",
+      },
+    ],
   },
 };
 
