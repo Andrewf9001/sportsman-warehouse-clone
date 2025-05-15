7b1fbad5e0bb25d6322f1221877ee6c03c427c39
Andrewf9001 <andrewf9001@gmail.com>
2025-05-15 14:36:26 -0600

    added camping links base

--- src/helpers/links/camping.js
+++ src/helpers/links/camping.js
@@ -1,57 +1,344 @@
 const campingLinkData = {
   link: "Camping",
   subCategory: {
-    title: "",
+    title: "Tents & Shelters",
     links: [
       {
         link: "#",
-        text: "",
+        text: "Camping Tents",
+      },
+      {
+        link: "#",
+        text: "Canvas Tents",
+      },
+      {
+        link: "#",
+        text: "Backpacking Tents",
+      },
+      {
+        link: "#",
+        text: "Car & Truck Tents",
+      },
+      {
+        link: "#",
+        text: "Canopies",
+      },
+      {
+        link: "#",
+        text: "Footprints",
+      },
+      {
+        link: "#",
+        text: "Accessories",
       },
     ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Camp Furniture",
+    links: [
+      {
+        link: "#",
+        text: "Chairs",
+      },
+      {
+        link: "#",
+        text: "Tables",
+      },
+      {
+        link: "#",
+        text: "Hammocks",
+      },
+      {
+        link: "#",
+        text: "Fire Pits",
+      },
+      {
+        link: "#",
+        text: "Camp Hygiene",
+      },
+      {
+        link: "#",
+        text: "Heaters",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Backpacks",
+    links: [
+      {
+        link: "#",
+        text: "Backpacking Packs",
+      },
+      {
+        link: "#",
+        text: "Day Packs",
+      },
+      {
+        link: "#",
+        text: "Hunting Packs",
+      },
+      {
+        link: "#",
+        text: "Hydration Packs",
+      },
+      {
+        link: "#",
+        text: "Waist Packs",
+      },
+      {
+        link: "#",
+        text: "Child Backpack Carriers",
+      },
+      {
+        link: "#",
+        text: "Duffel Bags & Luggage",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Sleeping Gear",
+    links: [
+      {
+        link: "#",
+        text: "Sleeping Bags",
+      },
+      {
+        link: "#",
+        text: "Sleeping Pads",
+      },
+      {
+        link: "#",
+        text: "Cots",
+      },
+      {
+        link: "#",
+        text: "Air Mattresses",
+      },
+      {
+        link: "#",
+        text: "Pillows, Blankets, & Liners",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Camp Kitchen",
+    links: [
+      {
+        link: "#",
+        text: "Grills & Camp Stoves",
+      },
+      {
+        link: "#",
+        text: "Backpacking Stoves",
+      },
+      {
+        link: "#",
+        text: "Dutch Oven & Cast Iron",
+      },
+      {
+        link: "#",
+        text: "Cookware",
+      },
+      {
+        link: "#",
+        text: "Dishes & Utensils",
+      },
+      {
+        link: "#",
+        text: "Food & Nutrition",
+      },
+      {
+        link: "#",
+        text: "Cooking Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Coolers & Storage",
+    links: [
+      {
+        link: "#",
+        text: "Hard Coolers",
+      },
+      {
+        link: "#",
+        text: "Soft Coolers",
+      },
+      {
+        link: "#",
+        text: "Cooler Accessories",
+      },
+      {
+        link: "#",
+        text: "Storage",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Hydration",
+    links: [
+      {
+        link: "#",
+        text: "Hydration Packs",
+      },
+      {
+        link: "#",
+        text: "Cups & Tumblers",
+      },
+      {
+        link: "#",
+        text: "Water Storage",
+      },
+      {
+        link: "#",
+        text: "Water Filtration",
+      },
+      {
+        link: "#",
+        text: "Water Bottles",
+      },
+      {
+        link: "#",
+        text: "Travel Mugs & Thermoses",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Safety & Survival",
+    links: [
+      {
+        link: "#",
+        text: "Generators",
+      },
+      {
+        link: "#",
+        text: "Power Stations & Solar",
+      },
+      {
+        link: "#",
+        text: "First Aid",
+      },
+      {
+        link: "#",
+        text: "Emergency & Survival Kits",
+      },
+      {
+        link: "#",
+        text: "Bug & Sun",
+      },
+      {
+        link: "#",
+        text: "Pepper & Bear Spray",
+      },
+      {
+        link: "#",
+        text: "Compasses & Maps",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Lighting",
+    links: [
+      {
+        link: "#",
+        text: "Headlamps",
+      },
+      {
+        link: "#",
+        text: "Flashlights",
+      },
+      {
+        link: "#",
+        text: "Lanterns",
+      },
+      {
+        link: "#",
+        text: "Spotlights",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Knives & Tools",
+    links: [
+      {
+        link: "#",
+        text: "Knives",
+      },
+      {
+        link: "#",
+        text: "Multi-tools",
+      },
+      {
+        link: "#",
+        text: "Axes & Tools",
+      },
+      {
+        link: "#",
+        text: "Replacement Blades",
+      },
+      {
+        link: "#",
+        text: "Knife Sharpeners",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Electronics",
+    links: [
+      {
+        link: "#",
+        text: "GPS",
+      },
+      {
+        link: "#",
+        text: "GPS Watches",
+      },
+      {
+        link: "#",
+        text: "Two-Way Radios",
+      },
+      {
+        link: "#",
+        text: "Metal Detectors",
+      },
+      {
+        link: "#",
+        text: "Weather Stations",
+      },
+      {
+        link: "#",
+        text: "Dog Collars",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Specialty",
+    links: [
+      {
+        link: "#",
+        text: "Auto & RV",
+      },
+      {
+        link: "#",
+        text: "Hobbies, Games, & Toys",
+      },
+      {
+        link: "#",
+        text: "Hiking Gear",
+      },
+      {
+        link: "#",
+        text: "Powersports",
+      },
+      {
+        link: "#",
+        text: "Dog Camping",
+      },
+      {
+        link: "#",
+        text: "Camp Novelties",
+      },
+    ],
   },
 };
 
