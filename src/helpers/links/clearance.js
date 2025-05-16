5955519df959de2e532cfa9a69f374986413f032
Andrewf9001 <andrewf9001@gmail.com>
2025-05-16 16:33:41 -0600

    added clearance links base

--- src/helpers/links/clearance.js
+++ src/helpers/links/clearance.js
@@ -1,41 +1,228 @@
 const clearanceLinkData = {
   link: "Clearance",
   subCategory: {
-    title: "",
+    title: "Shooting",
     links: [
       {
         link: "#",
-        text: "",
+        text: "Guns",
+      },
+      {
+        link: "#",
+        text: "Optics",
+      },
+      {
+        link: "#",
+        text: "Ammo",
+      },
+      {
+        link: "#",
+        text: "Accessories",
       },
     ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Camping",
+    links: [
+      {
+        link: "#",
+        text: "Tents",
+      },
+      {
+        link: "#",
+        text: "Backpacks",
+      },
+      {
+        link: "#",
+        text: "Sleeping Bags",
+      },
+      {
+        link: "#",
+        text: "Camp Chairs",
+      },
+      {
+        link: "#",
+        text: "Coolers",
+      },
+      {
+        link: "#",
+        text: "Knives",
+      },
+      {
+        link: "#",
+        text: "Outdoor Cooking",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Hunting",
+    links: [
+      {
+        link: "#",
+        text: "Optics",
+      },
+      {
+        link: "#",
+        text: "Archery",
+      },
+      {
+        link: "#",
+        text: "Decoys",
+      },
+      {
+        link: "#",
+        text: "Blinds",
+      },
+      {
+        link: "#",
+        text: "Calls",
+      },
+      {
+        link: "#",
+        text: "Hunting Accessories",
+      },
+      {
+        link: "#",
+        text: "Camo",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Fishing",
+    links: [
+      {
+        link: "#",
+        text: "Rods",
+      },
+      {
+        link: "#",
+        text: "Reels",
+      },
+      {
+        link: "#",
+        text: "Rod Reel Combos",
+      },
+      {
+        link: "#",
+        text: "Lures & Bait",
+      },
+      {
+        link: "#",
+        text: "Tackle",
+      },
+      {
+        link: "#",
+        text: "Fly Fishing",
+      },
+      {
+        link: "#",
+        text: "Fishing Shirts",
+      },
+      {
+        link: "#",
+        text: "Waders",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Boating",
+    links: [
+      {
+        link: "#",
+        text: "Kayaks",
+      },
+      {
+        link: "#",
+        text: "Electronics",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Clothing",
+    links: [
+      {
+        link: "#",
+        text: "Men's Outerwear",
+      },
+      {
+        link: "#",
+        text: "Men's Shirts",
+      },
+      {
+        link: "#",
+        text: "Men's Pants & Shorts",
+      },
+      {
+        link: "#",
+        text: "Camo",
+      },
+      {
+        link: "#",
+        text: "Men's Accessories",
+      },
+      {
+        link: "#",
+        text: "Women's Outerwear",
+      },
+      {
+        link: "#",
+        text: "Women's Shirts",
+      },
+      {
+        link: "#",
+        text: "Women's Pants & Shorts",
+      },
+      {
+        link: "#",
+        text: "Women's Accessories",
+      },
+      {
+        link: "#",
+        text: "Sunglasses",
+      },
+      {
+        link: "#",
+        text: "Youth",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Footwear",
+    links: [
+      {
+        link: "#",
+        text: "Men's Boots",
+      },
+      {
+        link: "#",
+        text: "Men's Shoes",
+      },
+      {
+        link: "#",
+        text: "Women's Boots",
+      },
+      {
+        link: "#",
+        text: "Women's Shoes",
+      },
+      {
+        link: "#",
+        text: "Youth",
+      },
+      {
+        link: "#",
+        text: "Socks & Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Deals & Events",
+    links: [
+      {
+        link: "#",
+        text: "Rebates",
+      },
+    ],
   },
 };
 
