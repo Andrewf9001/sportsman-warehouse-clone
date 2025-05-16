15e4c9564c9c48e06869882e25559c261d1d58e8
Andrewf9001 <andrewf9001@gmail.com>
2025-05-16 16:16:42 -0600

    added footwear links base

--- src/helpers/links/footwear.js
+++ src/helpers/links/footwear.js
@@ -1,41 +1,232 @@
 const footwearLinkData = {
   link: "Footwear",
   subCategory: {
-    title: "",
+    title: "Men's Outdoor",
     links: [
       {
         link: "#",
-        text: "",
+        text: "Hunting Boots",
+      },
+      {
+        link: "#",
+        text: "Hiking Boots",
+      },
+      {
+        link: "#",
+        text: "Work Boots",
+      },
+      {
+        link: "#",
+        text: "Western Boots",
+      },
+      {
+        link: "#",
+        text: "Rubber Boots",
+      },
+      {
+        link: "#",
+        text: "Winter Boots",
+      },
+      {
+        link: "#",
+        text: "Snowshoes",
       },
     ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Men's Shoes",
+    links: [
+      {
+        link: "#",
+        text: "Casual Shoes",
+      },
+      {
+        link: "#",
+        text: "Athletic Shoes",
+      },
+      {
+        link: "#",
+        text: "Sandals",
+      },
+      {
+        link: "#",
+        text: "Boat & Water Shoes",
+      },
+      {
+        link: "#",
+        text: "Slippers",
+      },
+      {
+        link: "#",
+        text: "Socks",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Women's Outdoor",
+    links: [
+      {
+        link: "#",
+        text: "Hunting Boots",
+      },
+      {
+        link: "#",
+        text: "Hiking Boots",
+      },
+      {
+        link: "#",
+        text: "Work Boots",
+      },
+      {
+        link: "#",
+        text: "Rubber Boots",
+      },
+      {
+        link: "#",
+        text: "Winter Boots",
+      },
+      {
+        link: "#",
+        text: "Snowshoes",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Women's Shoes",
+    links: [
+      {
+        link: "#",
+        text: "Casual Shoes",
+      },
+      {
+        link: "#",
+        text: "Athletic Shoes",
+      },
+      {
+        link: "#",
+        text: "Sandals & Water Shoes",
+      },
+      {
+        link: "#",
+        text: "Slippers",
+      },
+      {
+        link: "#",
+        text: "Socks",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Youth",
+    links: [
+      {
+        link: "#",
+        text: "Hunting Boots",
+      },
+      {
+        link: "#",
+        text: "Hiking & Casual",
+      },
+      {
+        link: "#",
+        text: "Rubber Boots",
+      },
+      {
+        link: "#",
+        text: "Sandals & Watershoes",
+      },
+      {
+        link: "#",
+        text: "Winter Boots",
+      },
+      {
+        link: "#",
+        text: "Socks",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Waders & Wading Boots",
+    links: [
+      {
+        link: "#",
+        text: "Men's Fishing Waders",
+      },
+      {
+        link: "#",
+        text: "Men's Hunting Waders",
+      },
+      {
+        link: "#",
+        text: "Women's Waders",
+      },
+      {
+        link: "#",
+        text: "Youth Waders",
+      },
+      {
+        link: "#",
+        text: "Wading Accessories",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Accessories",
+    links: [
+      {
+        link: "#",
+        text: "Insoles",
+      },
+      {
+        link: "#",
+        text: "Laces",
+      },
+      {
+        link: "#",
+        text: "Waterproofing & Care",
+      },
+      {
+        link: "#",
+        text: "Boot Dryers",
+      },
+      {
+        link: "#",
+        text: "Ice Cleats",
+      },
+      {
+        link: "#",
+        text: "Gaiters",
+      },
+    ],
   },
   subCategory: {
-    title: "",
-    links: [],
+    title: "Top Brands",
+    links: [
+      {
+        link: "#",
+        text: "Crispi",
+      },
+      {
+        link: "#",
+        text: "Zamberlan",
+      },
+      {
+        link: "#",
+        text: "Danner",
+      },
+      {
+        link: "#",
+        text: "LaCrosse",
+      },
+      {
+        link: "#",
+        text: "Hey Dude",
+      },
+      {
+        link: "#",
+        text: "Ariat",
+      },
+    ],
   },
 };
 
