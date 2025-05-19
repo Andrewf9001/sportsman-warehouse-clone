2dc096cb03f481589a90fcb2fbc5675b9ce816fc
Andrewf9001 <andrewf9001@gmail.com>
2025-05-19 13:32:52 -0600

    updated clearance links subcategory -> subcategories

--- src/helpers/links/clearance.js
+++ src/helpers/links/clearance.js
@@ -1,229 +1,231 @@
 const clearanceLinkData = {
   link: "Clearance",
-  subCategory: {
-    title: "Shooting",
-    links: [
-      {
-        link: "#",
-        text: "Guns",
-      },
-      {
-        link: "#",
-        text: "Optics",
-      },
-      {
-        link: "#",
-        text: "Ammo",
-      },
-      {
-        link: "#",
-        text: "Accessories",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Camping",
-    links: [
-      {
-        link: "#",
-        text: "Tents",
-      },
-      {
-        link: "#",
-        text: "Backpacks",
-      },
-      {
-        link: "#",
-        text: "Sleeping Bags",
-      },
-      {
-        link: "#",
-        text: "Camp Chairs",
-      },
-      {
-        link: "#",
-        text: "Coolers",
-      },
-      {
-        link: "#",
-        text: "Knives",
-      },
-      {
-        link: "#",
-        text: "Outdoor Cooking",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Hunting",
-    links: [
-      {
-        link: "#",
-        text: "Optics",
-      },
-      {
-        link: "#",
-        text: "Archery",
-      },
-      {
-        link: "#",
-        text: "Decoys",
-      },
-      {
-        link: "#",
-        text: "Blinds",
-      },
-      {
-        link: "#",
-        text: "Calls",
-      },
-      {
-        link: "#",
-        text: "Hunting Accessories",
-      },
-      {
-        link: "#",
-        text: "Camo",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Fishing",
-    links: [
-      {
-        link: "#",
-        text: "Rods",
-      },
-      {
-        link: "#",
-        text: "Reels",
-      },
-      {
-        link: "#",
-        text: "Rod Reel Combos",
-      },
-      {
-        link: "#",
-        text: "Lures & Bait",
-      },
-      {
-        link: "#",
-        text: "Tackle",
-      },
-      {
-        link: "#",
-        text: "Fly Fishing",
-      },
-      {
-        link: "#",
-        text: "Fishing Shirts",
-      },
-      {
-        link: "#",
-        text: "Waders",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Boating",
-    links: [
-      {
-        link: "#",
-        text: "Kayaks",
-      },
-      {
-        link: "#",
-        text: "Electronics",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Clothing",
-    links: [
-      {
-        link: "#",
-        text: "Men's Outerwear",
-      },
-      {
-        link: "#",
-        text: "Men's Shirts",
-      },
-      {
-        link: "#",
-        text: "Men's Pants & Shorts",
-      },
-      {
-        link: "#",
-        text: "Camo",
-      },
-      {
-        link: "#",
-        text: "Men's Accessories",
-      },
-      {
-        link: "#",
-        text: "Women's Outerwear",
-      },
-      {
-        link: "#",
-        text: "Women's Shirts",
-      },
-      {
-        link: "#",
-        text: "Women's Pants & Shorts",
-      },
-      {
-        link: "#",
-        text: "Women's Accessories",
-      },
-      {
-        link: "#",
-        text: "Sunglasses",
-      },
-      {
-        link: "#",
-        text: "Youth",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Footwear",
-    links: [
-      {
-        link: "#",
-        text: "Men's Boots",
-      },
-      {
-        link: "#",
-        text: "Men's Shoes",
-      },
-      {
-        link: "#",
-        text: "Women's Boots",
-      },
-      {
-        link: "#",
-        text: "Women's Shoes",
-      },
-      {
-        link: "#",
-        text: "Youth",
-      },
-      {
-        link: "#",
-        text: "Socks & Accessories",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Deals & Events",
-    links: [
-      {
-        link: "#",
-        text: "Rebates",
-      },
-    ],
-  },
+  subCategories: [
+    {
+      title: "Shooting",
+      links: [
+        {
+          link: "#",
+          text: "Guns",
+        },
+        {
+          link: "#",
+          text: "Optics",
+        },
+        {
+          link: "#",
+          text: "Ammo",
+        },
+        {
+          link: "#",
+          text: "Accessories",
+        },
+      ],
+    },
+    {
+      title: "Camping",
+      links: [
+        {
+          link: "#",
+          text: "Tents",
+        },
+        {
+          link: "#",
+          text: "Backpacks",
+        },
+        {
+          link: "#",
+          text: "Sleeping Bags",
+        },
+        {
+          link: "#",
+          text: "Camp Chairs",
+        },
+        {
+          link: "#",
+          text: "Coolers",
+        },
+        {
+          link: "#",
+          text: "Knives",
+        },
+        {
+          link: "#",
+          text: "Outdoor Cooking",
+        },
+      ],
+    },
+    {
+      title: "Hunting",
+      links: [
+        {
+          link: "#",
+          text: "Optics",
+        },
+        {
+          link: "#",
+          text: "Archery",
+        },
+        {
+          link: "#",
+          text: "Decoys",
+        },
+        {
+          link: "#",
+          text: "Blinds",
+        },
+        {
+          link: "#",
+          text: "Calls",
+        },
+        {
+          link: "#",
+          text: "Hunting Accessories",
+        },
+        {
+          link: "#",
+          text: "Camo",
+        },
+      ],
+    },
+    {
+      title: "Fishing",
+      links: [
+        {
+          link: "#",
+          text: "Rods",
+        },
+        {
+          link: "#",
+          text: "Reels",
+        },
+        {
+          link: "#",
+          text: "Rod Reel Combos",
+        },
+        {
+          link: "#",
+          text: "Lures & Bait",
+        },
+        {
+          link: "#",
+          text: "Tackle",
+        },
+        {
+          link: "#",
+          text: "Fly Fishing",
+        },
+        {
+          link: "#",
+          text: "Fishing Shirts",
+        },
+        {
+          link: "#",
+          text: "Waders",
+        },
+      ],
+    },
+    {
+      title: "Boating",
+      links: [
+        {
+          link: "#",
+          text: "Kayaks",
+        },
+        {
+          link: "#",
+          text: "Electronics",
+        },
+      ],
+    },
+    {
+      title: "Clothing",
+      links: [
+        {
+          link: "#",
+          text: "Men's Outerwear",
+        },
+        {
+          link: "#",
+          text: "Men's Shirts",
+        },
+        {
+          link: "#",
+          text: "Men's Pants & Shorts",
+        },
+        {
+          link: "#",
+          text: "Camo",
+        },
+        {
+          link: "#",
+          text: "Men's Accessories",
+        },
+        {
+          link: "#",
+          text: "Women's Outerwear",
+        },
+        {
+          link: "#",
+          text: "Women's Shirts",
+        },
+        {
+          link: "#",
+          text: "Women's Pants & Shorts",
+        },
+        {
+          link: "#",
+          text: "Women's Accessories",
+        },
+        {
+          link: "#",
+          text: "Sunglasses",
+        },
+        {
+          link: "#",
+          text: "Youth",
+        },
+      ],
+    },
+    {
+      title: "Footwear",
+      links: [
+        {
+          link: "#",
+          text: "Men's Boots",
+        },
+        {
+          link: "#",
+          text: "Men's Shoes",
+        },
+        {
+          link: "#",
+          text: "Women's Boots",
+        },
+        {
+          link: "#",
+          text: "Women's Shoes",
+        },
+        {
+          link: "#",
+          text: "Youth",
+        },
+        {
+          link: "#",
+          text: "Socks & Accessories",
+        },
+      ],
+    },
+    {
+      title: "Deals & Events",
+      links: [
+        {
+          link: "#",
+          text: "Rebates",
+        },
+      ],
+    },
+  ],
 };
 
 export default clearanceLinkData;
