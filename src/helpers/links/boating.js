95bdc2ccac17df241d01bb8a1ad42938bc5e5be8
Andrewf9001 <andrewf9001@gmail.com>
2025-05-19 13:30:55 -0600

    updated boating subcategory -> subcategories structure

--- src/helpers/links/boating.js
+++ src/helpers/links/boating.js
@@ -1,171 +1,173 @@
 const boatingLinkData = {
   link: "Boating",
-  subCategory: {
-    title: "Kayaks",
-    links: [
-      {
-        link: "#",
-        text: "Inflatable Kayaks",
-      },
-      {
-        link: "#",
-        text: "Sit On Top Kayaks",
-      },
-      {
-        link: "#",
-        text: "Sit In Kayaks",
-      },
-      {
-        link: "#",
-        text: "Fishing Kayaks",
-      },
-      {
-        link: "#",
-        text: "Pedal Kayaks",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Inflatables",
-    links: [
-      {
-        link: "#",
-        text: "Rafts",
-      },
-      {
-        link: "#",
-        text: "Packrafts",
-      },
-      {
-        link: "#",
-        text: "Paddle Boards",
-      },
-      {
-        link: "#",
-        text: "Float Tubes & Pontoons",
-      },
-      {
-        link: "#",
-        text: "Water Toys & Accessories",
-      },
-      {
-        link: "#",
-        text: "Towables & Tubes",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Essentials",
-    links: [
-      {
-        link: "#",
-        text: "Life Jackets",
-      },
-      {
-        link: "#",
-        text: "Paddles & Oars",
-      },
-      {
-        link: "#",
-        text: "Dry Bags",
-      },
-      {
-        link: "#",
-        text: "Sunglasses",
-      },
-      {
-        link: "#",
-        text: "Storage & Transport",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Boat Accessories",
-    links: [
-      {
-        link: "#",
-        text: "Safety",
-      },
-      {
-        link: "#",
-        text: "Trailer Accessories",
-      },
-      {
-        link: "#",
-        text: "Anchoring & Docking",
-      },
-      {
-        link: "#",
-        text: "Boat Seats",
-      },
-      {
-        link: "#",
-        text: "Boat Covers",
-      },
-      {
-        link: "#",
-        text: "Fuel Cans",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Canoes",
-    links: [],
-  },
-  subCategory: {
-    title: "Boat Equipment",
-    links: [
-      {
-        link: "#",
-        text: "Marine Electronics",
-      },
-      {
-        link: "#",
-        text: "Trolling Motors",
-      },
-      {
-        link: "#",
-        text: "Gas Motors",
-      },
-      {
-        link: "#",
-        text: "Rod Holders",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Specialty",
-    links: [
-      {
-        link: "#",
-        text: "Women's Sandals & Water Shoes",
-      },
-      {
-        link: "#",
-        text: "Men's Boat & Water Shoes",
-      },
-      {
-        link: "#",
-        text: "Sun Apparel",
-      },
-      {
-        link: "#",
-        text: "Shade Shelters",
-      },
-      {
-        link: "#",
-        text: "Coolers",
-      },
-      {
-        link: "#",
-        text: "Chairs",
-      },
-      {
-        link: "#",
-        text: "Bug & Sun Protection",
-      },
-    ],
-  },
+  subCategories: [
+    {
+      title: "Kayaks",
+      links: [
+        {
+          link: "#",
+          text: "Inflatable Kayaks",
+        },
+        {
+          link: "#",
+          text: "Sit On Top Kayaks",
+        },
+        {
+          link: "#",
+          text: "Sit In Kayaks",
+        },
+        {
+          link: "#",
+          text: "Fishing Kayaks",
+        },
+        {
+          link: "#",
+          text: "Pedal Kayaks",
+        },
+      ],
+    },
+    {
+      title: "Inflatables",
+      links: [
+        {
+          link: "#",
+          text: "Rafts",
+        },
+        {
+          link: "#",
+          text: "Packrafts",
+        },
+        {
+          link: "#",
+          text: "Paddle Boards",
+        },
+        {
+          link: "#",
+          text: "Float Tubes & Pontoons",
+        },
+        {
+          link: "#",
+          text: "Water Toys & Accessories",
+        },
+        {
+          link: "#",
+          text: "Towables & Tubes",
+        },
+      ],
+    },
+    {
+      title: "Essentials",
+      links: [
+        {
+          link: "#",
+          text: "Life Jackets",
+        },
+        {
+          link: "#",
+          text: "Paddles & Oars",
+        },
+        {
+          link: "#",
+          text: "Dry Bags",
+        },
+        {
+          link: "#",
+          text: "Sunglasses",
+        },
+        {
+          link: "#",
+          text: "Storage & Transport",
+        },
+      ],
+    },
+    {
+      title: "Boat Accessories",
+      links: [
+        {
+          link: "#",
+          text: "Safety",
+        },
+        {
+          link: "#",
+          text: "Trailer Accessories",
+        },
+        {
+          link: "#",
+          text: "Anchoring & Docking",
+        },
+        {
+          link: "#",
+          text: "Boat Seats",
+        },
+        {
+          link: "#",
+          text: "Boat Covers",
+        },
+        {
+          link: "#",
+          text: "Fuel Cans",
+        },
+      ],
+    },
+    {
+      title: "Canoes",
+      links: [],
+    },
+    {
+      title: "Boat Equipment",
+      links: [
+        {
+          link: "#",
+          text: "Marine Electronics",
+        },
+        {
+          link: "#",
+          text: "Trolling Motors",
+        },
+        {
+          link: "#",
+          text: "Gas Motors",
+        },
+        {
+          link: "#",
+          text: "Rod Holders",
+        },
+      ],
+    },
+    {
+      title: "Specialty",
+      links: [
+        {
+          link: "#",
+          text: "Women's Sandals & Water Shoes",
+        },
+        {
+          link: "#",
+          text: "Men's Boat & Water Shoes",
+        },
+        {
+          link: "#",
+          text: "Sun Apparel",
+        },
+        {
+          link: "#",
+          text: "Shade Shelters",
+        },
+        {
+          link: "#",
+          text: "Coolers",
+        },
+        {
+          link: "#",
+          text: "Chairs",
+        },
+        {
+          link: "#",
+          text: "Bug & Sun Protection",
+        },
+      ],
+    },
+  ],
 };
 
 export default boatingLinkData;
