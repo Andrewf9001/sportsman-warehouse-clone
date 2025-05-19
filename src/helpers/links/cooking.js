bc1544a96b701098f1087e0e845f715f3bbbbe4e
Andrewf9001 <andrewf9001@gmail.com>
2025-05-19 13:34:49 -0600

    updated cooking + fishing subcategory -> subcategories

--- src/helpers/links/cooking.js
+++ src/helpers/links/cooking.js
@@ -1,113 +1,115 @@
 const cookingLinkData = {
   link: "Cooking",
-  subCategory: {
-    title: "Grills & Camp Stoves",
-    links: [
-      {
-        link: "#",
-        text: "BBQ Grills",
-      },
-      {
-        link: "#",
-        text: "Camp Stoves",
-      },
-      {
-        link: "#",
-        text: "Griddles",
-      },
-      {
-        link: "#",
-        text: "Grill Boxes & Over Fire Grills",
-      },
-      {
-        link: "#",
-        text: "Grilling Accessories",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Pellet Grills & Smokers",
-    links: [
-      {
-        link: "#",
-        text: "Smokers",
-      },
-      {
-        link: "#",
-        text: "Pellets & Wood",
-      },
-      {
-        link: "#",
-        text: "Pellet Grill & Smoker Accessories",
-      },
-      {
-        link: "#",
-        text: "Pellet Grills",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Tools & Accessories",
-    links: [
-      {
-        link: "#",
-        text: "Cookware",
-      },
-      {
-        link: "#",
-        text: "Cooking Accessories",
-      },
-      {
-        link: "#",
-        text: "Canopies & Shelters",
-      },
-      {
-        link: "#",
-        text: "Covers & Carrying Cases",
-      },
-      {
-        link: "#",
-        text: "Campfire Accessories",
-      },
-      {
-        link: "#",
-        text: "Tables & Chairs",
-      },
-      {
-        link: "#",
-        text: "Dishes & Utensils",
-      },
-      {
-        link: "#",
-        text: "Beverages & Dessert Making",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Specialty",
-    links: [
-      {
-        link: "#",
-        text: "Dutch Oven & Cast Iron",
-      },
-      {
-        link: "#",
-        text: "Backpacking Stoves",
-      },
-      {
-        link: "#",
-        text: "Food Processing",
-      },
-      {
-        link: "#",
-        text: "Rubs & Spices",
-      },
-      {
-        link: "#",
-        text: "Fryers & Oils",
-      },
-    ],
-  },
+  subCategories: [
+    {
+      title: "Grills & Camp Stoves",
+      links: [
+        {
+          link: "#",
+          text: "BBQ Grills",
+        },
+        {
+          link: "#",
+          text: "Camp Stoves",
+        },
+        {
+          link: "#",
+          text: "Griddles",
+        },
+        {
+          link: "#",
+          text: "Grill Boxes & Over Fire Grills",
+        },
+        {
+          link: "#",
+          text: "Grilling Accessories",
+        },
+      ],
+    },
+    {
+      title: "Pellet Grills & Smokers",
+      links: [
+        {
+          link: "#",
+          text: "Smokers",
+        },
+        {
+          link: "#",
+          text: "Pellets & Wood",
+        },
+        {
+          link: "#",
+          text: "Pellet Grill & Smoker Accessories",
+        },
+        {
+          link: "#",
+          text: "Pellet Grills",
+        },
+      ],
+    },
+    {
+      title: "Tools & Accessories",
+      links: [
+        {
+          link: "#",
+          text: "Cookware",
+        },
+        {
+          link: "#",
+          text: "Cooking Accessories",
+        },
+        {
+          link: "#",
+          text: "Canopies & Shelters",
+        },
+        {
+          link: "#",
+          text: "Covers & Carrying Cases",
+        },
+        {
+          link: "#",
+          text: "Campfire Accessories",
+        },
+        {
+          link: "#",
+          text: "Tables & Chairs",
+        },
+        {
+          link: "#",
+          text: "Dishes & Utensils",
+        },
+        {
+          link: "#",
+          text: "Beverages & Dessert Making",
+        },
+      ],
+    },
+    {
+      title: "Specialty",
+      links: [
+        {
+          link: "#",
+          text: "Dutch Oven & Cast Iron",
+        },
+        {
+          link: "#",
+          text: "Backpacking Stoves",
+        },
+        {
+          link: "#",
+          text: "Food Processing",
+        },
+        {
+          link: "#",
+          text: "Rubs & Spices",
+        },
+        {
+          link: "#",
+          text: "Fryers & Oils",
+        },
+      ],
+    },
+  ],
 };
 
 export default cookingLinkData;
--- src/helpers/links/fishing.js
+++ src/helpers/links/fishing.js
@@ -1,317 +1,319 @@
 const fishingLinkData = {
   link: "Fishing",
-  subCategory: {
-    title: "Rods",
-    links: [
-      {
-        link: "#",
-        text: "Spinning Rods",
-      },
-      {
-        link: "#",
-        text: "Casting Rods",
-      },
-      {
-        link: "#",
-        text: "Trolling Rods",
-      },
-      {
-        link: "#",
-        text: "Fly Rods",
-      },
-      {
-        link: "#",
-        text: "Saltwater Rods",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Reels",
-    links: [
-      {
-        link: "#",
-        text: "Spinning Reels",
-      },
-      {
-        link: "#",
-        text: "Casting Reels",
-      },
-      {
-        link: "#",
-        text: "Trolling & Conventional Reels",
-      },
-      {
-        link: "#",
-        text: "Fly Reels",
-      },
-      {
-        link: "#",
-        text: "Spincast Reels",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Rod & Reel Combos",
-    links: [
-      {
-        link: "#",
-        text: "Spinning Combos",
-      },
-      {
-        link: "#",
-        text: "Casting Combos",
-      },
-      {
-        link: "#",
-        text: "Trolling Combos",
-      },
-      {
-        link: "#",
-        text: "Fly Fishing Combos",
-      },
-      {
-        link: "#",
-        text: "Saltwater Combos",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Lures & Bait",
-    links: [
-      {
-        link: "#",
-        text: "Soft Baits",
-      },
-      {
-        link: "#",
-        text: "Hard Baits",
-      },
-      {
-        link: "#",
-        text: "Jigs & Rigs",
-      },
-      {
-        link: "#",
-        text: "Bait & Attractants",
-      },
-      {
-        link: "#",
-        text: "Lure Building",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Tackle Storage",
-    links: [
-      {
-        link: "#",
-        text: "Hard Tackle Boxes",
-      },
-      {
-        link: "#",
-        text: "Soft Tackle Boxes",
-      },
-      {
-        link: "#",
-        text: "Fishing Packs",
-      },
-      {
-        link: "#",
-        text: "Fishing Vests",
-      },
-      {
-        link: "#",
-        text: "Fishing Bags",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Terminal Tackle",
-    links: [
-      {
-        link: "#",
-        text: "Fishing Line",
-      },
-      {
-        link: "#",
-        text: "Leaders & Accessories",
-      },
-      {
-        link: "#",
-        text: "Hooks",
-      },
-      {
-        link: "#",
-        text: "Sinkers",
-      },
-      {
-        link: "#",
-        text: "Swivels & Snaps",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Fishing Accessories",
-    links: [
-      {
-        link: "#",
-        text: "Fishing Nets",
-      },
-      {
-        link: "#",
-        text: "Fillet Knives",
-      },
-      {
-        link: "#",
-        text: "Pliers & Tools",
-      },
-      {
-        link: "#",
-        text: "Scales & Rulers",
-      },
-      {
-        link: "#",
-        text: "Stringers & Fish Keepers",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Boat Equipment",
-    links: [
-      {
-        link: "#",
-        text: "Fish Finders",
-      },
-      {
-        link: "#",
-        text: "Trolling Motors",
-      },
-      {
-        link: "#",
-        text: "Downriggers & Accessories",
-      },
-      {
-        link: "#",
-        text: "Marine Radios",
-      },
-      {
-        link: "#",
-        text: "Mapping Software",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Saltwater Fishing",
-    links: [
-      {
-        link: "#",
-        text: "Saltwater Combos",
-      },
-      {
-        link: "#",
-        text: "Saltwater Rods",
-      },
-      {
-        link: "#",
-        text: "Saltwater Reels",
-      },
-      {
-        link: "#",
-        text: "Saltwater Lures",
-      },
-      {
-        link: "#",
-        text: "Saltwater Terminal Tackle",
-      },
-      {
-        link: "#",
-        text: "Saltwater Gear & Accessories",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Fly Fishing",
-    links: [
-      {
-        link: "#",
-        text: "Fly Fishing Combos",
-      },
-      {
-        link: "#",
-        text: "Fly Rods",
-      },
-      {
-        link: "#",
-        text: "Fly Reels",
-      },
-      {
-        link: "#",
-        text: "Fly Line, Leaders, & Tippet",
-      },
-      {
-        link: "#",
-        text: "Flies",
-      },
-      {
-        link: "#",
-        text: "Fly Tying",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Ice Fishing",
-    links: [
-      {
-        link: "#",
-        text: "Tents",
-      },
-      {
-        link: "#",
-        text: "Sleds",
-      },
-      {
-        link: "#",
-        text: "Ice Augers",
-      },
-      {
-        link: "#",
-        text: "Ice Fishing Electronics",
-      },
-      {
-        link: "#",
-        text: "Rods, Reels, & Combos",
-      },
-      {
-        link: "#",
-        text: "Ice Fishing Clothing",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Specialty Fishing",
-    links: [
-      {
-        link: "#",
-        text: "Trolling Gear",
-      },
-      {
-        link: "#",
-        text: "Crabbing & Clamming",
-      },
-      {
-        link: "#",
-        text: "Float Tubes & Pontoons",
-      },
-      {
-        link: "#",
-        text: "Bow Fishing",
-      },
-      {
-        link: "#",
-        text: "Bait Traps & Farming",
-      },
-    ],
-  },
+  subCategories: [
+    {
+      title: "Rods",
+      links: [
+        {
+          link: "#",
+          text: "Spinning Rods",
+        },
+        {
+          link: "#",
+          text: "Casting Rods",
+        },
+        {
+          link: "#",
+          text: "Trolling Rods",
+        },
+        {
+          link: "#",
+          text: "Fly Rods",
+        },
+        {
+          link: "#",
+          text: "Saltwater Rods",
+        },
+      ],
+    },
+    {
+      title: "Reels",
+      links: [
+        {
+          link: "#",
+          text: "Spinning Reels",
+        },
+        {
+          link: "#",
+          text: "Casting Reels",
+        },
+        {
+          link: "#",
+          text: "Trolling & Conventional Reels",
+        },
+        {
+          link: "#",
+          text: "Fly Reels",
+        },
+        {
+          link: "#",
+          text: "Spincast Reels",
+        },
+      ],
+    },
+    {
+      title: "Rod & Reel Combos",
+      links: [
+        {
+          link: "#",
+          text: "Spinning Combos",
+        },
+        {
+          link: "#",
+          text: "Casting Combos",
+        },
+        {
+          link: "#",
+          text: "Trolling Combos",
+        },
+        {
+          link: "#",
+          text: "Fly Fishing Combos",
+        },
+        {
+          link: "#",
+          text: "Saltwater Combos",
+        },
+      ],
+    },
+    {
+      title: "Lures & Bait",
+      links: [
+        {
+          link: "#",
+          text: "Soft Baits",
+        },
+        {
+          link: "#",
+          text: "Hard Baits",
+        },
+        {
+          link: "#",
+          text: "Jigs & Rigs",
+        },
+        {
+          link: "#",
+          text: "Bait & Attractants",
+        },
+        {
+          link: "#",
+          text: "Lure Building",
+        },
+      ],
+    },
+    {
+      title: "Tackle Storage",
+      links: [
+        {
+          link: "#",
+          text: "Hard Tackle Boxes",
+        },
+        {
+          link: "#",
+          text: "Soft Tackle Boxes",
+        },
+        {
+          link: "#",
+          text: "Fishing Packs",
+        },
+        {
+          link: "#",
+          text: "Fishing Vests",
+        },
+        {
+          link: "#",
+          text: "Fishing Bags",
+        },
+      ],
+    },
+    {
+      title: "Terminal Tackle",
+      links: [
+        {
+          link: "#",
+          text: "Fishing Line",
+        },
+        {
+          link: "#",
+          text: "Leaders & Accessories",
+        },
+        {
+          link: "#",
+          text: "Hooks",
+        },
+        {
+          link: "#",
+          text: "Sinkers",
+        },
+        {
+          link: "#",
+          text: "Swivels & Snaps",
+        },
+      ],
+    },
+    {
+      title: "Fishing Accessories",
+      links: [
+        {
+          link: "#",
+          text: "Fishing Nets",
+        },
+        {
+          link: "#",
+          text: "Fillet Knives",
+        },
+        {
+          link: "#",
+          text: "Pliers & Tools",
+        },
+        {
+          link: "#",
+          text: "Scales & Rulers",
+        },
+        {
+          link: "#",
+          text: "Stringers & Fish Keepers",
+        },
+      ],
+    },
+    {
+      title: "Boat Equipment",
+      links: [
+        {
+          link: "#",
+          text: "Fish Finders",
+        },
+        {
+          link: "#",
+          text: "Trolling Motors",
+        },
+        {
+          link: "#",
+          text: "Downriggers & Accessories",
+        },
+        {
+          link: "#",
+          text: "Marine Radios",
+        },
+        {
+          link: "#",
+          text: "Mapping Software",
+        },
+      ],
+    },
+    {
+      title: "Saltwater Fishing",
+      links: [
+        {
+          link: "#",
+          text: "Saltwater Combos",
+        },
+        {
+          link: "#",
+          text: "Saltwater Rods",
+        },
+        {
+          link: "#",
+          text: "Saltwater Reels",
+        },
+        {
+          link: "#",
+          text: "Saltwater Lures",
+        },
+        {
+          link: "#",
+          text: "Saltwater Terminal Tackle",
+        },
+        {
+          link: "#",
+          text: "Saltwater Gear & Accessories",
+        },
+      ],
+    },
+    {
+      title: "Fly Fishing",
+      links: [
+        {
+          link: "#",
+          text: "Fly Fishing Combos",
+        },
+        {
+          link: "#",
+          text: "Fly Rods",
+        },
+        {
+          link: "#",
+          text: "Fly Reels",
+        },
+        {
+          link: "#",
+          text: "Fly Line, Leaders, & Tippet",
+        },
+        {
+          link: "#",
+          text: "Flies",
+        },
+        {
+          link: "#",
+          text: "Fly Tying",
+        },
+      ],
+    },
+    {
+      title: "Ice Fishing",
+      links: [
+        {
+          link: "#",
+          text: "Tents",
+        },
+        {
+          link: "#",
+          text: "Sleds",
+        },
+        {
+          link: "#",
+          text: "Ice Augers",
+        },
+        {
+          link: "#",
+          text: "Ice Fishing Electronics",
+        },
+        {
+          link: "#",
+          text: "Rods, Reels, & Combos",
+        },
+        {
+          link: "#",
+          text: "Ice Fishing Clothing",
+        },
+      ],
+    },
+    {
+      title: "Specialty Fishing",
+      links: [
+        {
+          link: "#",
+          text: "Trolling Gear",
+        },
+        {
+          link: "#",
+          text: "Crabbing & Clamming",
+        },
+        {
+          link: "#",
+          text: "Float Tubes & Pontoons",
+        },
+        {
+          link: "#",
+          text: "Bow Fishing",
+        },
+        {
+          link: "#",
+          text: "Bait Traps & Farming",
+        },
+      ],
+    },
+  ],
 };
 
 export default fishingLinkData;
