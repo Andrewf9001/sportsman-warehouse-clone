0b8ee38fef48545daef5a6f29a70b5d8c3db8e8d
Andrewf9001 <andrewf9001@gmail.com>
2025-05-19 13:33:38 -0600

    updated clothing subcategory -> subcategories

--- src/helpers/links/clothing.js
+++ src/helpers/links/clothing.js
@@ -1,369 +1,371 @@
 const clothingLinkData = {
   link: "Clothing",
-  subCategory: {
-    title: "Men's Hunting",
-    links: [
-      {
-        link: "#",
-        text: "Hunting Coats & Jackets",
-      },
-      {
-        link: "#",
-        text: "Hunting Vests",
-      },
-      {
-        link: "#",
-        text: "Hunting Shirts",
-      },
-      {
-        link: "#",
-        text: "Hunting Pants",
-      },
-      {
-        link: "#",
-        text: "Hunting Waders",
-      },
-      {
-        link: "#",
-        text: "Blaze Orange",
-      },
-      {
-        link: "#",
-        text: "Shop by Game",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Men's Workwear",
-    links: [
-      {
-        link: "#",
-        text: "Coats & Jackets",
-      },
-      {
-        link: "#",
-        text: "Hoodies & Sweatshirts",
-      },
-      {
-        link: "#",
-        text: "Shirts",
-      },
-      {
-        link: "#",
-        text: "Pants & Shorts",
-      },
-      {
-        link: "#",
-        text: "Vests",
-      },
-      {
-        link: "#",
-        text: "Bibs & Coveralls",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Men's Fishing",
-    links: [
-      {
-        link: "#",
-        text: "Coats & Jackets",
-      },
-      {
-        link: "#",
-        text: "Shirts",
-      },
-      {
-        link: "#",
-        text: "Pants & Shorts",
-      },
-      {
-        link: "#",
-        text: "Bibs",
-      },
-      {
-        link: "#",
-        text: "Vests",
-      },
-      {
-        link: "#",
-        text: "Waders",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Men's Casual",
-    links: [
-      {
-        link: "#",
-        text: "Coats & Jackets",
-      },
-      {
-        link: "#",
-        text: "Hoodies & Sweatshirts",
-      },
-      {
-        link: "#",
-        text: "Rainwear",
-      },
-      {
-        link: "#",
-        text: "Shirts",
-      },
-      {
-        link: "#",
-        text: "Pants",
-      },
-      {
-        link: "#",
-        text: "Shorts",
-      },
-      {
-        link: "#",
-        text: "Baselayer",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Women's Hunting",
-    links: [
-      {
-        link: "#",
-        text: "Hunting Jackets & Vests",
-      },
-      {
-        link: "#",
-        text: "Hunting Shirts",
-      },
-      {
-        link: "#",
-        text: "Hunting Pants",
-      },
-      {
-        link: "#",
-        text: "Hunting Waders",
-      },
-      {
-        link: "#",
-        text: "Blaze Orange",
-      },
-      {
-        link: "#",
-        text: "Shop by Game",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Women's Workwear",
-    links: [
-      {
-        link: "#",
-        text: "Coats & Jackets",
-      },
-      {
-        link: "#",
-        text: "Hoodies & Sweatshirts",
-      },
-      {
-        link: "#",
-        text: "Shirts",
-      },
-      {
-        link: "#",
-        text: "Pants",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Women's Fishing",
-    links: [
-      {
-        link: "#",
-        text: "Shirts & Jackets",
-      },
-      {
-        link: "#",
-        text: "Pants & Shorts",
-      },
-      {
-        link: "#",
-        text: "Bibs",
-      },
-      {
-        link: "#",
-        text: "Vests",
-      },
-      {
-        link: "#",
-        text: "Waders",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Women's Casual",
-    links: [
-      {
-        link: "#",
-        text: "Coats & Jackets",
-      },
-      {
-        link: "#",
-        text: "Hoodies & Sweatshirts",
-      },
-      {
-        link: "#",
-        text: "Rainwear",
-      },
-      {
-        link: "#",
-        text: "Shirts",
-      },
-      {
-        link: "#",
-        text: "Pants & Shorts",
-      },
-      {
-        link: "#",
-        text: "Baselayer",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Youth",
-    links: [
-      {
-        link: "#",
-        text: "Hunting & Fishing",
-      },
-      {
-        link: "#",
-        text: "Rainwear",
-      },
-      {
-        link: "#",
-        text: "Outerwear",
-      },
-      {
-        link: "#",
-        text: "Shirts",
-      },
-      {
-        link: "#",
-        text: "Pants & Shorts",
-      },
-      {
-        link: "#",
-        text: "Baselayer",
-      },
-      {
-        link: "#",
-        text: "Blaze Orange",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Big & Tall",
-    links: [
-      {
-        link: "#",
-        text: "Hunting",
-      },
-      {
-        link: "#",
-        text: "Workwear",
-      },
-      {
-        link: "#",
-        text: "Fishing",
-      },
-      {
-        link: "#",
-        text: "Rainwear",
-      },
-      {
-        link: "#",
-        text: "Casual",
-      },
-      {
-        link: "#",
-        text: "Women's Plus Sizes",
-      },
-    ],
-  },
-  subCategory: {
-    title: "Accessories",
-    links: [
-      {
-        link: "#",
-        text: "Socks",
-      },
-      {
-        link: "#",
-        text: "Gloves",
-      },
-      {
-        link: "#",
-        text: "Hats",
-      },
-      {
-        link: "#",
-        text: "Sunglasses",
-      },
-      {
-        link: "#",
-        text: "Watches",
-      },
-      {
-        link: "#",
-        text: "Belts",
-      },
-      {
-        link: "#",
-        text: "Gaiters",
-      },
-      {
-        link: "#",
-        text: "Handbags",
-      },
-    ],
-  },
-  subCategory: {
-    title: "",
-    links: [
-      {
-        link: "#",
-        text: "Sitka",
-      },
-      {
-        link: "#",
-        text: "Killik",
-      },
-      {
-        link: "#",
-        text: "King's Camo",
-      },
-      {
-        link: "#",
-        text: "Rustic Ridge",
-      },
-      {
-        link: "#",
-        text: "Carhartt",
-      },
-      {
-        link: "#",
-        text: "Ariat",
-      },
-      {
-        link: "#",
-        text: "Simms",
-      },
-      {
-        link: "#",
-        text: "Huk",
-      },
-    ],
-  },
+  subCategories: [
+    {
+      title: "Men's Hunting",
+      links: [
+        {
+          link: "#",
+          text: "Hunting Coats & Jackets",
+        },
+        {
+          link: "#",
+          text: "Hunting Vests",
+        },
+        {
+          link: "#",
+          text: "Hunting Shirts",
+        },
+        {
+          link: "#",
+          text: "Hunting Pants",
+        },
+        {
+          link: "#",
+          text: "Hunting Waders",
+        },
+        {
+          link: "#",
+          text: "Blaze Orange",
+        },
+        {
+          link: "#",
+          text: "Shop by Game",
+        },
+      ],
+    },
+    {
+      title: "Men's Workwear",
+      links: [
+        {
+          link: "#",
+          text: "Coats & Jackets",
+        },
+        {
+          link: "#",
+          text: "Hoodies & Sweatshirts",
+        },
+        {
+          link: "#",
+          text: "Shirts",
+        },
+        {
+          link: "#",
+          text: "Pants & Shorts",
+        },
+        {
+          link: "#",
+          text: "Vests",
+        },
+        {
+          link: "#",
+          text: "Bibs & Coveralls",
+        },
+      ],
+    },
+    {
+      title: "Men's Fishing",
+      links: [
+        {
+          link: "#",
+          text: "Coats & Jackets",
+        },
+        {
+          link: "#",
+          text: "Shirts",
+        },
+        {
+          link: "#",
+          text: "Pants & Shorts",
+        },
+        {
+          link: "#",
+          text: "Bibs",
+        },
+        {
+          link: "#",
+          text: "Vests",
+        },
+        {
+          link: "#",
+          text: "Waders",
+        },
+      ],
+    },
+    {
+      title: "Men's Casual",
+      links: [
+        {
+          link: "#",
+          text: "Coats & Jackets",
+        },
+        {
+          link: "#",
+          text: "Hoodies & Sweatshirts",
+        },
+        {
+          link: "#",
+          text: "Rainwear",
+        },
+        {
+          link: "#",
+          text: "Shirts",
+        },
+        {
+          link: "#",
+          text: "Pants",
+        },
+        {
+          link: "#",
+          text: "Shorts",
+        },
+        {
+          link: "#",
+          text: "Baselayer",
+        },
+      ],
+    },
+    {
+      title: "Women's Hunting",
+      links: [
+        {
+          link: "#",
+          text: "Hunting Jackets & Vests",
+        },
+        {
+          link: "#",
+          text: "Hunting Shirts",
+        },
+        {
+          link: "#",
+          text: "Hunting Pants",
+        },
+        {
+          link: "#",
+          text: "Hunting Waders",
+        },
+        {
+          link: "#",
+          text: "Blaze Orange",
+        },
+        {
+          link: "#",
+          text: "Shop by Game",
+        },
+      ],
+    },
+    {
+      title: "Women's Workwear",
+      links: [
+        {
+          link: "#",
+          text: "Coats & Jackets",
+        },
+        {
+          link: "#",
+          text: "Hoodies & Sweatshirts",
+        },
+        {
+          link: "#",
+          text: "Shirts",
+        },
+        {
+          link: "#",
+          text: "Pants",
+        },
+      ],
+    },
+    {
+      title: "Women's Fishing",
+      links: [
+        {
+          link: "#",
+          text: "Shirts & Jackets",
+        },
+        {
+          link: "#",
+          text: "Pants & Shorts",
+        },
+        {
+          link: "#",
+          text: "Bibs",
+        },
+        {
+          link: "#",
+          text: "Vests",
+        },
+        {
+          link: "#",
+          text: "Waders",
+        },
+      ],
+    },
+    {
+      title: "Women's Casual",
+      links: [
+        {
+          link: "#",
+          text: "Coats & Jackets",
+        },
+        {
+          link: "#",
+          text: "Hoodies & Sweatshirts",
+        },
+        {
+          link: "#",
+          text: "Rainwear",
+        },
+        {
+          link: "#",
+          text: "Shirts",
+        },
+        {
+          link: "#",
+          text: "Pants & Shorts",
+        },
+        {
+          link: "#",
+          text: "Baselayer",
+        },
+      ],
+    },
+    {
+      title: "Youth",
+      links: [
+        {
+          link: "#",
+          text: "Hunting & Fishing",
+        },
+        {
+          link: "#",
+          text: "Rainwear",
+        },
+        {
+          link: "#",
+          text: "Outerwear",
+        },
+        {
+          link: "#",
+          text: "Shirts",
+        },
+        {
+          link: "#",
+          text: "Pants & Shorts",
+        },
+        {
+          link: "#",
+          text: "Baselayer",
+        },
+        {
+          link: "#",
+          text: "Blaze Orange",
+        },
+      ],
+    },
+    {
+      title: "Big & Tall",
+      links: [
+        {
+          link: "#",
+          text: "Hunting",
+        },
+        {
+          link: "#",
+          text: "Workwear",
+        },
+        {
+          link: "#",
+          text: "Fishing",
+        },
+        {
+          link: "#",
+          text: "Rainwear",
+        },
+        {
+          link: "#",
+          text: "Casual",
+        },
+        {
+          link: "#",
+          text: "Women's Plus Sizes",
+        },
+      ],
+    },
+    {
+      title: "Accessories",
+      links: [
+        {
+          link: "#",
+          text: "Socks",
+        },
+        {
+          link: "#",
+          text: "Gloves",
+        },
+        {
+          link: "#",
+          text: "Hats",
+        },
+        {
+          link: "#",
+          text: "Sunglasses",
+        },
+        {
+          link: "#",
+          text: "Watches",
+        },
+        {
+          link: "#",
+          text: "Belts",
+        },
+        {
+          link: "#",
+          text: "Gaiters",
+        },
+        {
+          link: "#",
+          text: "Handbags",
+        },
+      ],
+    },
+    {
+      title: "",
+      links: [
+        {
+          link: "#",
+          text: "Sitka",
+        },
+        {
+          link: "#",
+          text: "Killik",
+        },
+        {
+          link: "#",
+          text: "King's Camo",
+        },
+        {
+          link: "#",
+          text: "Rustic Ridge",
+        },
+        {
+          link: "#",
+          text: "Carhartt",
+        },
+        {
+          link: "#",
+          text: "Ariat",
+        },
+        {
+          link: "#",
+          text: "Simms",
+        },
+        {
+          link: "#",
+          text: "Huk",
+        },
+      ],
+    },
+  ],
 };
 
 export default clothingLinkData;
