207609e64172cf5520148c89d15433f4ecc377cf
Andrewf9001 <andrewf9001@gmail.com>
2025-05-20 13:46:04 -0600

    setup jsx for mobile search, setup base styles

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,11 +1,21 @@
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div className="search-bar-container">
-      <input type="search" />
-      <FontAwesomeIcon icon={faMagnifyingGlass} />
+    <div className={`search-bar-container ${isExpanded ? "expanded" : ""}`}>
+      <input type="search" placeholder="Search..." />
+
+      <button
+        className="search-icon"
+        label="Toggle search"
+        onClick={() => setIsExpanded((prev) => !prev)}
+      >
+        <FontAwesomeIcon icon={isExpanded ? faX : faMagnifyingGlass} />
+      </button>
     </div>
   );
 };
