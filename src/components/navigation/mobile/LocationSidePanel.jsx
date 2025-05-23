5b6156e32127b79d9d2f911380c34c6c88deee38
Andrewf9001 <andrewf9001@gmail.com>
2025-05-23 14:08:12 -0600

    created location side panel

---
+++ src/components/navigation/mobile/LocationSidePanel.jsx
@@ -0,0 +1,9 @@
+const LocationSidePanel = () => {
+  return (
+    <div className="location-side-panel-container">
+      <h1>Location Side Panel</h1>
+    </div>
+  );
+};
+
+export default LocationSidePanel;
